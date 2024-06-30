574f61ecf00e4be9dd0d629dcb4345a0078c9d2e
Gennadios <egon.sch@yandex.ru>
2024-06-30 17:31:36 -0500

    Create nav bar component

--- use-popcorn/src/App.js
+++ use-popcorn/src/App.js
@@ -1,7 +1,22 @@
 import { tempMovieData, tempWatchedData } from './data/static';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return <NavBar />;
+}
+
+function NavBar() {
+  return (
+    <nav className='nav-bar'>
+      <div className='logo'>
+        <span role='img'>🍿</span>
+        <h1>usePopcorn</h1>
+      </div>
+      <input className='search' type='text' placeholder='Search movies...' />
+      <p className='num-results'>
+        Found <strong>[NUM]</strong> results
+      </p>
+    </nav>
+  );
 }
 
 export default App;
