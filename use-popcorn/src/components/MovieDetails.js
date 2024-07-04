b0f055c25d89bd3fc6cab3126d8d5d1d072cadb3
Gennadios <egon.sch@yandex.ru>
2024-07-04 11:21:44 -0500

    Add dynamic data fetching for MovieDetails and styling

--- use-popcorn/src/components/MovieDetails.js
+++ use-popcorn/src/components/MovieDetails.js
@@ -1,10 +1,78 @@
+import { useEffect, useState } from 'react';
+import Loader from './Loader';
+import StarRating from './StarRating';
+
 function MovieDetails({ selectedId, onCloseMovieDetails }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+    imdbRating,
+  } = movie;
+
+  console.log(title, year);
+
+  useEffect(
+    function () {
+      setIsLoading(true);
+      async function getMovieDetails() {
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+      }
+      getMovieDetails();
+      setIsLoading(false);
+    },
+    [selectedId]
+  );
+
   return (
-    <div className='detail'>
-      <button class='btn-back' onClick={onCloseMovieDetails}>
-        &larr;
-      </button>
-      {selectedId}
+    <div className='details'>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className='btn-back' onClick={onCloseMovieDetails}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of the movie ${movie}`} />
+            <div className='details-overview'>
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating} IMDb Rating
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className='rating'>
+              <StarRating maxRating={10} fontSizePx={18} />
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring: {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
